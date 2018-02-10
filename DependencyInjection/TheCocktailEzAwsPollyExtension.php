3309a18f145447f585f46ee6be42f804f56993e6
crevillo <crevillo@gmail.com>
2018-02-10 21:59:26 +0100

    Create dedicate fieldtype for storing polly files

--- DependencyInjection/TheCocktailEzAwsPollyExtension.php
+++ DependencyInjection/TheCocktailEzAwsPollyExtension.php
@@ -3,12 +3,31 @@
 namespace TheCocktail\EzAwsPollyBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Yaml\Yaml;
 
-class TheCocktailEzAwsPollyExtension extends Extension
+class TheCocktailEzAwsPollyExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container)
+    {
+        $activatedBundles = array_keys($container->getParameter('kernel.bundles'));
+        if (in_array('eZPlatformUIBundle', $activatedBundles, true)) {
+            $configs['platformui/yui.yaml'] = 'ez_platformui';
+        }
+
+
+        foreach ($configs as $fileName => $extensionName) {
+            $configFile = __DIR__ . '/../Resources/config/' . $fileName;
+            $config = Yaml::parse(file_get_contents($configFile));
+            $container->prependExtensionConfig($extensionName, $config);
+            $container->addResource(new FileResource($configFile));
+        }
+    }
+
     /**
      * {@inheritdoc}
      */
---
+++ Resources/public/js/views/field/thecocktail-awspollyfile-edit-view.js
@@ -0,0 +1,147 @@
+YUI.add('thecocktail-awspollyfile-field-edit-view', function (Y) {
+
+    Y.namespace('theCocktail');
+
+    var FIELDTYPE_IDENTIFIER = 'ezawspollyfile';
+
+    Y.theCocktail.AwsPollyFileEditView = Y.Base.create('awspollyfileEditView', Y.eZ.BinaryBaseEditView, [], {
+        initializer: function () {
+            this._handleFieldDescriptionVisibility = false;
+            this.after('fileChange', this._uiBinaryFileChange);
+        },
+
+        /**
+         * Reflects the new binaryfile object in the generated UI
+         *
+         * @method _uiBinaryFileChange
+         * @param {EventFacade} e the binaryfile change event facade
+         * @protected
+         */
+        _uiBinaryFileChange: function (e) {
+            var binaryfile = this.get('file'),
+                container = this.get('container'),
+                removeButton = container.one('.ez-button-delete');
+
+            if ( e.prevVal ) {
+                this._setTypeClasses(false, e.prevVal);
+            }
+            if ( binaryfile ) {
+                container.one('.ez-binaryfile-properties-name').setContent(binaryfile.name);
+                container.one('.ez-binaryfile-properties-size').setContent(binaryfile.size);
+                container.one('.ez-binaryfile-properties-type').setContent(binaryfile.type);
+                container.one('.ez-binaryfile-download').setAttribute('href', binaryfile.uri);
+                removeButton.set('disabled', false);
+            } else {
+                // no need to update the DOM, the binaryfile is hidden by CSS
+                removeButton.set('disabled', true);
+            }
+            this._setStateClasses();
+            this.validate();
+        },
+
+
+        /**
+         * Adds or removes classes based on the mimetype of the file currently
+         * selected
+         *
+         * @method _setTypeClasses
+         * @param {Boolean} add whether to add or remove the class
+         * @param {Object} binaryfile the binary file struct
+         * @protected
+         */
+        _setTypeClasses: function (add, binaryfile) {
+            var container = this.get('container'),
+                mimeTypeClass, mimeSubTypeClass;
+
+            if ( !binaryfile.type ) {
+                return;
+            }
+
+            mimeSubTypeClass = this._getMimeTypeClass(binaryfile.type);
+            mimeTypeClass = this._getTypeClass(binaryfile.type);
+            if ( add ) {
+                container.addClass(mimeTypeClass).addClass(mimeSubTypeClass);
+            } else {
+                container.removeClass(mimeTypeClass).removeClass(mimeSubTypeClass);
+            }
+        },
+
+        /**
+         * Returns the class reflecting the full mime type. The subtype part is
+         * sanitized so that we generate a valid class
+         *
+         * @method _getMimeTypeClass
+         * @param {String} mimeType the mime type ("text/plain" for instance)
+         * @return String
+         * @private
+         */
+        _getMimeTypeClass: function (mimeType) {
+            var parts = mimeType.split('/'),
+                subType = parts[1].replace(/[^a-zA-Z0-9]/g, '-');
+
+            return 'is-mimetype-' + parts[0] + '-' + subType;
+        },
+
+        /**
+         * Returns the class reflecting the type part of mime type. (ie "text"
+         * in "text/plain").
+         *
+         * @method _getTypeClass
+         * @param {String} mimeType the mime type ("text/plain" for instance)
+         * @return {String}
+         * @private
+         */
+        _getTypeClass: function (mimeType) {
+            var parts = mimeType.split('/');
+
+            return 'is-type-' + parts[0];
+        },
+
+        /**
+         * Set the state classes on the view container
+         *
+         * @method _setStateClasses
+         * @protected
+         */
+        _setStateClasses: function () {
+            this.constructor.superclass._setStateClasses.call(this);
+            if ( !this._isEmpty() ) {
+                this._setTypeClasses(true, this.get('file'));
+            }
+        },
+
+        /**
+         * Defines the variables to be imported in the field edit template.
+         *
+         * @protected
+         * @method _variables
+         * @return {Object}
+         */
+        _variables: function () {
+            return {
+                "isRequired": this.get('fieldDefinition').isRequired,
+                "binaryfile": this.get('file'),
+            };
+        },
+
+        /**
+         * Removes the `url` property from the fieldValue so that the fieldValue
+         * represents a valid BinaryFile\Value object.
+         *
+         * @method _completeFieldValue
+         * @param {Object} fieldValue
+         * @protected
+         * @return {Object}
+         */
+        _completeFieldValue: function (fieldValue) {
+            delete fieldValue.url;
+            delete fieldValue.mimeType;
+
+            return fieldValue;
+        },
+    });
+
+    Y.eZ.FieldEditView.registerFieldEditView(
+        FIELDTYPE_IDENTIFIER, Y.theCocktail.AwsPollyFileEditView
+    );
+});
