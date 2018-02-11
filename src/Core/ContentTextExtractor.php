a4176c33264c0e911ce32f9002ad5995d5ef8741
crevillo <crevillo@gmail.com>
2018-02-11 18:13:40 +0100

    aproach to a better management of ssml transformations

--- src/Core/ContentTextExtractor.php
+++ src/Core/ContentTextExtractor.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace TheCocktail\EzAwsPollyBundle\Transformer;
+namespace TheCocktail\EzAwsPollyBundle\Core;
 
-use eZ\Publish\API\Repository\ContentService;
+use eZ\Publish\API\Repository\ContentTypeService;
 use eZ\Publish\API\Repository\Values\Content\Content;
+use eZ\Publish\Core\MVC\Symfony\Controller\Content\ViewController;
+use eZ\Publish\Core\MVC\Symfony\View\ContentView;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
-use TheCocktail\Aws\Polly\Speech;
 
-class SSMLTransformer
+class ContentTextExtractor
 {
     protected $httpKernel;
 
@@ -19,13 +20,15 @@ class SSMLTransformer
 
     public function extractTextFromContent(Content $content)
     {
-        $request = Request::create('/view/content/' . $content->id . '/ssml');
+        $request =  Request::create('/view/content/' . $content->id . '/ssml');
 
         $response = $this->httpKernel->handle(
             $request,
             HttpKernelInterface::SUB_REQUEST
         );
 
-        return $response->getContent();
+
+        print_r($response->getContent());
+        die;
     }
 }
